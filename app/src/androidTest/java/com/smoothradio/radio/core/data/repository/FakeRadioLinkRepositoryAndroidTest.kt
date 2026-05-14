486a51c3ae5dd5a97d0cb2594b35d3fafed6eab6
markgithinji <57723258+markgithinji@users.noreply.github.com>
2026-05-14 05:52:20 +0300

    test: update FakeRadioLinkRepository with RemoteAdSettings support and emission helpers

--- app/src/androidTest/java/com/smoothradio/radio/core/data/repository/FakeRadioLinkRepositoryAndroidTest.kt
+++ app/src/androidTest/java/com/smoothradio/radio/core/data/repository/FakeRadioLinkRepositoryAndroidTest.kt
@@ -1,5 +1,6 @@
 package com.smoothradio.radio.core.data.repository
 
+import com.smoothradio.radio.core.domain.model.RemoteAdSettings
 import com.smoothradio.radio.core.domain.repository.RadioLinkRepository
 import com.smoothradio.radio.core.util.RadioStationLinksHelper
 import com.smoothradio.radio.core.util.Resource
@@ -14,9 +15,23 @@ class FakeRadioLinkRepositoryAndroidTest : RadioLinkRepository {
         )
     )
 
+    private val _adSettingsFlow = MutableStateFlow<Resource<RemoteAdSettings>>(
+        Resource.Success(RemoteAdSettings(4, 4))
+    )
+
     var clearCalled = false // For verification in tests
 
     override fun getRemoteStreamLinksFlow(): Flow<Resource<List<String>>> = _linksFlow
+
+    override fun getRemoteAdSettingsFlow(): Flow<Resource<RemoteAdSettings>> = _adSettingsFlow
+
+    fun emitLinks(links: List<String>) {
+        _linksFlow.value = Resource.Success(links)
+    }
+
+    fun emitAdSettings(settings: RemoteAdSettings) {
+        _adSettingsFlow.value = Resource.Success(settings)
+    }
 
     override fun clear() {
         clearCalled = true
